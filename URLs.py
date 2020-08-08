7fac123a2e87a4cede3bb90420e53cb9fbce1c04
mshevdrdin123 <mshevdrdin123@gmail.com>
2020-08-08 23:41:00 +0300

    create manage.py and URL.py

---
+++ URLs.py
@@ -0,0 +1,2 @@
+token = '1339433216:AAG2yVyCWeUYe-A0O-1hwvksvrM4xC3Epi8'
+URL = f'https://api.telegram.org/bot{token}/'
---
+++ manage.py
@@ -0,0 +1,13 @@
+import requests
+import URLs
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+def last_update(data):  
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+
